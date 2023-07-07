1bd1eec1e0515afac2f5c9b38a23744731d00bcc
AndresFernandez <73385513+Fernandezaginer@users.noreply.github.com>
2023-07-07 19:33:26 +0200

    Create aux.js

---
+++ aux.js
@@ -0,0 +1,32 @@
+var a2 = '   <img alt="workmap" height="613" src="https://blogger.googleusercontent.com/img/a/AVvXsEgTFx3E1mWt-5DrqSywPGa6BvCUrcwsHsW9yi3IX2CiHel0rBeB03sAYD1qIBomSUEJhdtQah_HTeOsK0hI1u6a3XYFv4YEJ7mHzFZsfs15_0h2ENJEHQsqDoS3QikaSjZEH3xFN_LKC_Gkw8xMTuQWEcNOxtcy022vQY6T_6okyrz924qkpZMEd66L-AQ=w519-h613" usemap="#workmap" width="519" />'
+var a3 = '      <map name="workmap">'
+var a4 = '        <area coords="107,579,156,430,238,431,241,581" href="https://drive.google.com/file/d/1HF8t_tlVriE7qELByGKerh-zkiKl9sF6/view?usp=drive_link" shape="poly"> </area>'
+var a5 = '        <area coords="191,429,221,364,347,373,456,521,241,582,239,431" href="https://drive.google.com/file/d/1TZ4zSnrsM8WQ3zqnwNmw98Qs12Dx7wdE/view?usp=drive_link" shape="poly"> </area>'
+var a6 = '        <area coords="129,507,29,379,119,321,148,347,119,389,157,432" href="https://drive.google.com/file/d/1VlRJxl064vLc7-nyIvPMucSCfykS-sGu/view?usp=drive_link" shape="poly"> </area>'
+var a7 = '        <area coords="156,430,120,389,201,268,248,303,191,429" href="https://drive.google.com/file/d/1rV7eUt2A0pzWpQ6Dppe33RdD6rlBglBA/view?usp=drive_link" shape="poly"> </area>'
+var a8 = '        <area coords="220,364,267,267,345,328,332,372" href="https://drive.google.com/file/d/1PyNTZc-24Yk8gLvVvZvynIrfbuMaXElz/view?usp=drive_link" shape="poly"> </area>'
+var a9 = '        <area coords="332,372,380,264,511,287,354,379" href="https://drive.google.com/file/d/1ljzLRIuOqwuFztz2geuI1Mmhbqy9vpsm/view?usp=drive_link" shape="poly"> </area>'
+var a10 = '        <area coords="32,378,80,220,179,204,199,268,146,342,118,323,29,377" href="https://drive.google.com/file/d/1PsiuCJZbLRRFf5UaMFxee9MCSCBaCrSW/view?usp=drive_link" shape="poly"> </area>'
+var a11 = '        <area coords="79,220,82,122,182,95,309,155,247,303,200,269,180,203" href="https://drive.google.com/file/d/1oF8_G6SEU34JPhNh3xnQIHGuZ_Sr4xcY/view?usp=drive_link" shape="poly"> </area>'
+var a12 = '        <area coords="267,270,311,156,380,263,348,330" href="https://drive.google.com/file/d/1W1szHoA-F5GyiLZCT0wSUngEevRDJLFc/view?usp=drive_link" shape="poly"> </area>'
+var a13 = '        <area coords="332,372,380,264,511,287,354,379" href="https://drive.google.com/file/d/1ljkDa9GDH4e94re9kotns-JInfhGdZ64/view?usp=drive_link" shape="poly"> </area>'
+var a14 = '        <area coords="309,155,279,93,432,47,458,133" href="https://drive.google.com/file/d/16sX2KL2622IdINbcIp2vQzSYeqnA_Q8O/view?usp=drive_link" shape="poly"> </area>'
+var a15 = '        <area coords="379,264,310,155,457,133,511,288" href="https://drive.google.com/file/d/1GBLadCwyzeV_Fw-T9u5RLTwFBfgFYPms/view?usp=drive_link" shape="poly"> </area>'
+var a16 = '        <area coords="309,155,181,94,154,13,277,9" href="https://drive.google.com/file/d/1ljkDa9GDH4e94re9kotns-JInfhGdZ64/view?usp=drive_link" shape="poly"> </area>'
+var a17 = '      </map>'
+document.write(a2)
+document.write(a3)
+document.write(a4)
+document.write(a5)
+document.write(a6)
+document.write(a7)
+document.write(a8)
+document.write(a9)
+document.write(a10)
+document.write(a11)
+document.write(a12)
+document.write(a13)
+document.write(a14)
+document.write(a15)
+document.write(a16)
+document.write(a17)
